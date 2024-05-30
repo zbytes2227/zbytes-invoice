d06b84b3413bb0b6158d0cb4bd975691f7f30060
zbytes2227 <zbytes0@gmail.com>
2024-05-30 13:32:32 +0530

    final

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "HandMakers",
+  title: "BharatGen CRM",
   description: "Invoice Software",
 };
 
