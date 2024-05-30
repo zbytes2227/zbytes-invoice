dab3423c77a5bd82676066a1be22d6fff34a222e
zbytes2227 <zbytes0@gmail.com>
2024-05-30 13:34:43 +0530

    final 2

--- components/Navbar.js
+++ components/Navbar.js
@@ -57,7 +57,7 @@ const Navbar = () => {
                   alt="FlowBite Logo"
                 />
                 <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  HandMakers
+                  BharatGen
                 </span>
               </a>
             </div>
